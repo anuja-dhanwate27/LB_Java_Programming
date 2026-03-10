4e6b9a67e1ac3ce491cdb1a529145bc4cf078b7b
Anuja Santosh Dhanwate <dhanwateanuja@gmail.com>
2026-03-10 12:40:32 +0530

    Add files via upload

---
+++ ClassWork/program878.java
@@ -0,0 +1,62 @@
+/*  
+   0   1   2   3     4   5     6   7   8   9 
+   a   b   c   d     e   f     g   h   i    j
+   97  98  99  100  101  102  103  
+
+*/
+import java.util.*;
+class program878
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        
+        System.out.println("Enter String : ");
+        String str = sobj.nextLine();
+
+        char Arr[] = str.toCharArray();
+
+        
+        HashMap <Character , Integer>hobj = new HashMap<Character , Integer>();
+
+        int old = 0;
+        for(char ch : Arr)
+        {
+          if(hobj.containsKey(ch))
+          {
+             old = hobj.get(ch);
+             hobj.put(ch,old+1);
+          }
+          else
+           {
+               hobj.put(ch, 1);
+
+           }
+        }
+        
+        int iMax = 0;
+        char cMax = '\0';
+
+        for(char ch : hobj.keySet())
+        {
+            if(hobj.get(ch) > iMax)
+            {
+                iMax = hobj.get(ch);
+                cMax = ch;
+
+            }
+           
+        }
+
+        System.out.println("Maximum time occured character is : "+cMax+" with thw frequecncy : "+iMax);
+
+
+       
+     
+        
+
+       
+        sobj.close();
+
+    }
+}
